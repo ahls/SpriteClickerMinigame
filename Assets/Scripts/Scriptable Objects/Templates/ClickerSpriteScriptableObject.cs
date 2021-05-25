bed8cd9aa35846dd4d3b28ac6e2374c3f186b8b3
ahls <ahls@uci.edu>
2021-05-24 21:20:04 -0700

    added the scriptable object added required scripts (not implemented)

---
+++ Assets/Scripts/Scriptable Objects/Templates/ClickerSpriteScriptableObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System;
+
+[Serializable]
+public class Movement
+{
+    public Vector2 velocity;
+    public float duration;
+    public bool doesNotBounce;
+}
+[CreateAssetMenu(fileName = "new Sprite", menuName = "Sprite Clicker SO")]
+public class ClickerSpriteScriptableObject : ScriptableObject
+{
+    public Movement[] movementList;
+    public Sprite sprite;
+    public float flahsingInterval;
+    public float flashingDuration;
+
+}
